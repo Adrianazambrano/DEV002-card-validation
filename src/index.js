43e9457641524117cc00d6b504f07365eff88ca0
Adriana Zambrano <adrianazambranoc7@gmail.com>
2022-11-02 22:15:15 -0500

    terminado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
-import validator from './validator.js';
 
-console.log(validator);
+
+//document.addEventListener('DOMContentLoaded', function () {
+
+    import validator from './validator.js';
+
+    console.log(validator);  
+
+    var formulario = document.getElementById('formulario');
+    var cardNumber = document.getElementById('numero');
+    var usuario = document.getElementById('nombre');
+    //console.log(cardNumber,usuario)
+
+// Capturar datos del formlario y mostrarlos en la tarjeta:
+
+    cardNumber.addEventListener("keyup", (event) => {
+        if (event.isComposing || event.keyCode === 229) {
+            return;
+        }
+       // document.getElementById("capturarNumero").value = cardNumber.value;
+       
+           });
+    usuario.addEventListener("keyup", (event) => {
+        if (event.isComposing || event.keyCode === 229) {
+             return;
+            }
+            document.getElementById("capturarNombre").value = usuario.value;
+               });  
+
+    formulario.addEventListener('submit', (event) => {
+        event.preventDefault();
+        var numeroValidar = cardNumber.value;
+        
+        const validacion = validator.isValid(numeroValidar);
+        const numeroOculto= validator.maskify(numeroValidar);
+        document.getElementById("capturarNumero").value = numeroOculto;
+
+        if ( validacion == true){
+             document.getElementById('mensaje').innerHTML= 'Numero de tarjeta valido';
+             document.getElementById('mensaje').style.color = '#008000';
+        
+
+    
+        } else{
+            document.getElementById('mensaje').innerHTML= 'Numero de tarjeta invalido';
+            document.getElementById('mensaje').style.color = '#FF0000';
+        }
+        
+        
+        console.log(validacion,numeroOculto) 
+        ;
+       });
+
+      
+                                 
+     
+     
+
+    
+    
+  
+	
+
+//}, false);
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,75 @@
 const validator = {
-  // ...
+  maskify: function (numeroValidar) {
+    return numeroValidar.slice(0, -4).replace(/./g, '#') + numeroValidar.slice(-4);
+   // return '############' + numeroValidar.slice(-4);
+    
+  },
+
+  isValid: function (numeroValidar) {
+    //1. pasar los numeros a un array en orden inverso:
+    var numeroInvertido = numeroValidar.toString().split('').reverse();
+    numeroInvertido = numeroInvertido.map(numero => {
+      return parseInt(numero)
+    })
+    console.log('paso1', numeroInvertido)
+    // 2. Operacion de posiciones pares:
+    var pares = [];
+    var impares = [];
+    for (let i = 0; i < numeroInvertido.length; i++) {
+      console.log('index', i)
+
+      if ((i+1) % 2 == 0) {
+        // multiplico el numero que este en la posicon par del array 
+        var numeroDoble = numeroInvertido[i] * 2;
+        // Descompongo el numero en dos cifras
+        var numero = numeroDoble.toString().split('');
+        // Sumo los numeros del numero descompuesto
+        const valorInicial = 0;
+        const suma = numero.reduce(
+          (valorAnterior, valorActual) => parseInt(valorAnterior) + parseInt(valorActual),
+          valorInicial
+        );
+
+        pares.push(suma);
+
+        //console.log(numeroDoble) 
+
+      } else {
+        impares.push(numeroInvertido[i])
+      }
+
+
+    }
+    console.log(pares, impares)
+    //  Union de array par e impar
+    var total = [...pares, ...impares]
+    //  Suma del resutado final 
+    const valorInicial = 0;
+    const suma = total.reduce(
+      (valorAnterior, valorActual) => (valorAnterior) + (valorActual),
+      valorInicial
+    );
+    console.log(suma)
+
+    return suma % 10 == 0;
+
+
+
+
+
+
+  }
+
+
+
+
 };
+
+// //validator.isValid(4095571174987314)
+// var hola = 4;
+// console.log(hola.toString().split(''))
+
+// console.log (validator.isValid(4095571174987314))
+
 
 export default validator;
